fc31cf6a2c962d300f09855ef34fbee90c2f40ce
MaoSal <121812173+MaoSal@users.noreply.github.com>
2024-11-04 21:14:57 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,4 @@
 import random
 
 word_list = ["mango", "banana", "blueberry", "blackberry", "grapes"]
-print(word_list)
-word = random.choice(word_list)
-print(word)
-guess = input("Enter a single letter: ")
 
-if len(guess) == 1 and guess.isalpha() == True:
-    print("Good guess!")
-else:
-    print("Oops!")
