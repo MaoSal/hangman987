869206b683f89e89e24f4e08732e86e0708c95e0
Marcio Salvador <msalv79@gmail.com>
2024-11-11 19:38:43 +0000

    File milestone_3.py ready

--- milestone_3.py
+++ milestone_3.py
@@ -1,2 +1,24 @@
 import random
 
+<<<<<<< HEAD
+=======
+word_list = ["mango", "banana", "blueberry", "blackberry", "grapes"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a single letter: ")
+        check_guess(guess)
+
+def check_guess(guess):
+    guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word {word}")
+    elif not guess in word:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        ask_for_input()
+
+ask_for_input()
+>>>>>>> 8d07182 (Added code to milestone_3.py)
